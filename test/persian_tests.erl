f606e1f2a25bfa80494425ed72f346af677d7b42
Unknown <vinicius.apk@gmail.com>
2016-10-03 09:05:26 -0300

    pendragon

--- test/persian_tests.erl
+++ test/persian_tests.erl
@@ -38,7 +38,6 @@ persian_qu_server:sync_get_msgs(Pid, 2).
 persian_qu_server:async_dequeue(Pid, 1).
 persian_qu_server:stop(Pid).
 
-
 f(Pid).
 {ok, Pid} = persian_qu_server:start().
 persian_qu_server:sync_enqueue(Pid, 1, 2).
@@ -49,3 +48,8 @@ persian_qu_server:sync_enqueue(Pid, 2, 6).
 persian_qu_server:sync_enqueue(Pid, 2, 7).
 persian_qu_server:sync_get_msgs(Pid, 1).
 persian_qu_server:stop(Pid).
+
+Clients = lists:seq(1, 10).
+Msgs = lists:seq(1, 100).
+X = lists:flatmap(fun(C) -> [{C, M} || M <- Msgs] end, Clients).
+[persian_qu_server:sync_enqueue(Pid, C, M) || {C, M} <- X].
